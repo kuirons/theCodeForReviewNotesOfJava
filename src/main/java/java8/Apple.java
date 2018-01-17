a82608404ff19281be52b4f337f43a0da0508c4c
kuirons <wuguangyu@popcornie.com>
2018-01-17 16:07:20 +0800

    添加junit测试框架

--- src/main/java/java8/Apple.java
+++ src/main/java/java8/Apple.java
@@ -5,68 +5,62 @@ import java.util.List;
 
 /**
  * @author WuGYu
- * @date 2018/1/12 11:47
- * 这也是策略模式的一个例子，将谓词抽象成多个独立的策略，可以适应更多的需求
+ * @date 2018/1/12 11:47 这也是策略模式的一个例子，将谓词抽象成多个独立的策略，可以适应更多的需求
  */
 public class Apple {
-    private int heavyWeight;
-    private String appleColor;
+  private int heavyWeight;
+  private String appleColor;
 
-    public Apple() {
-    }
+  public Apple() {}
 
-    public Apple(int i){
-        heavyWeight = i;
-    }
-    public String getAppleColor() {
-        return appleColor;
-    }
+  public Apple(int i) {
+    heavyWeight = i;
+  }
 
-    public void setAppleColor(String appleColor) {
-        this.appleColor = appleColor;
-    }
+  public String getAppleColor() {
+    return appleColor;
+  }
 
-    public int getHeavyWeight() {
+  public void setAppleColor(String appleColor) {
+    this.appleColor = appleColor;
+  }
 
-        return heavyWeight;
-    }
+  public int getHeavyWeight() {
 
-    public void setHeavyWeight(int heavyWeight) {
-        this.heavyWeight = heavyWeight;
-    }
+    return heavyWeight;
+  }
 
-    public static <T> List<T> applesFilter(List<T> inventory, ApplePredicate<T> predicate) {
-        List<T> result = new ArrayList<>();
-        for (T t :
-                inventory) {
-            if (predicate.test(t))
-                result.add(t);
-        }
-        return result;
-    }
+  public void setHeavyWeight(int heavyWeight) {
+    this.heavyWeight = heavyWeight;
+  }
 
-    public static void main(String[] args) {
-        List<Apple> testApple = new ArrayList<>();
-        Apple apple1 = new Apple();
-        apple1.setAppleColor("green");
-        apple1.setHeavyWeight(80);
-        Apple apple2 = new Apple();
-        apple2.setHeavyWeight(200);
-        apple2.setAppleColor("red");
-        testApple.add(apple1);
-        testApple.add(apple2);
-        for (Apple a :
-                Apple.applesFilter(testApple, (Apple a) -> "green".equals(a.getAppleColor()))) {
-            System.out.println(a.getAppleColor());
-        }
-        for (Apple a :
-                Apple.applesFilter(testApple, (Apple a) -> a.getHeavyWeight() > 150)) {
-            System.out.println(a.getHeavyWeight());
-        }
+  public static <T> List<T> applesFilter(List<T> inventory, ApplePredicate<T> predicate) {
+    List<T> result = new ArrayList<>();
+    for (T t : inventory) {
+      if (predicate.test(t)) result.add(t);
     }
+    return result;
+  }
+
+  public static void main(String[] args) {
+    List<Apple> testApple = new ArrayList<>();
+    Apple apple1 = new Apple();
+    apple1.setAppleColor("green");
+    apple1.setHeavyWeight(80);
+    Apple apple2 = new Apple();
+    apple2.setHeavyWeight(200);
+    apple2.setAppleColor("red");
+    testApple.add(apple1);
+    testApple.add(apple2);
+    for (Apple a : Apple.applesFilter(testApple, (Apple a) -> "green".equals(a.getAppleColor()))) {
+      System.out.println(a.getAppleColor());
+    }
+    for (Apple a : Apple.applesFilter(testApple, (Apple a) -> a.getHeavyWeight() > 150)) {
+      System.out.println(a.getHeavyWeight());
+    }
+  }
 }
 
 interface ApplePredicate<T> {
-    boolean test(T t);
+  boolean test(T t);
 }
-
---
+++ src/test/java8/AppleTest.java
@@ -0,0 +1,85 @@
+package java8; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Apple Tester. 
+* 
+* @author WuGYu
+* @since <pre>01/17/2018</pre> 
+* @version 1.0 
+*/ 
+public class AppleTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getAppleColor() 
+* 
+*/ 
+@Test
+public void testGetAppleColor() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setAppleColor(String appleColor) 
+* 
+*/ 
+@Test
+public void testSetAppleColor() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getHeavyWeight() 
+* 
+*/ 
+@Test
+public void testGetHeavyWeight() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setHeavyWeight(int heavyWeight) 
+* 
+*/ 
+@Test
+public void testSetHeavyWeight() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: applesFilter(List<T> inventory, ApplePredicate<T> predicate) 
+* 
+*/ 
+@Test
+public void testApplesFilter() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: main(String[] args) 
+* 
+*/ 
+@Test
+public void testMain() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
